f9c756330233104ba4e7695a3781159c8429e160
fuzioncloud <zzaharris@fuzioncloud.com>
2017-09-17 09:20:04 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -59,6 +59,6 @@ describe('sayHiToGrandmaAgain(string)', function() {
   })
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect("I love you, Grandma.".sayHiToGrandmaAgain()).toEqual("I love you, too.")
+    expect(.sayHiToGrandmaAgain().response()).toEqual("I love you, too.")
   })
 })
