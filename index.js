1f336844a8bbe8ae4f6ed61a991a6c98454a5f3a
fuzioncloud <zzaharris@fuzioncloud.com>
2017-09-17 08:44:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,17 +24,14 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandmaAgain(string) {
-  function response() {
     var message = "I'm not your grandma, fool! You better run on home to her though, before I make you my B****!\n ERROR MESSAGE ERROR MESSAGE ERROR MESSAGE\n If you are recieving this message you are not making the right calls in your if statement. REVIEW AND FIX!!!"
     if (string.toUpperCase() === string)
       message = "YES INDEED!"
-      return
     else if (string === string.toLowerCase())
       message = "I can\'t hear you!"
-      return
     else if (string === "I love you, Grandma.")
       message = "I love you, too."
-      return
+    return message;
   };
   return response();
 }
