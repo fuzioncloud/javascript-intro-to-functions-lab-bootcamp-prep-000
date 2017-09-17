b7dd7fcd74f2524586bce9e5573cc0e880088407
fuzioncloud <zzaharris@fuzioncloud.com>
2017-09-17 09:30:15 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -59,6 +59,6 @@ describe('sayHiToGrandmaAgain(string)', function() {
   })
 
   it('returns "I love you, too." if `string` is "I love you, Grandma."`', function() {
-    expect(response().sayHiToGrandmaAgain()).toEqual("I love you, too.")
+    expect(sayHiToGrandmaAgain(esponse())).toEqual("I love you, too.")
   })
 })
